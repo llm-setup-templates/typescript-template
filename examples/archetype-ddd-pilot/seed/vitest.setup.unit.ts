64a32e36d3651b30560985092580dc68a5201aa6
gs07103 <gwonseok02@gmail.com>
2026-05-02 09:57:20 +0900

    feat(seed): add vitest configs (workspace + setup unit/browser + shims) (Wave 1 T1.4)

---
+++ examples/archetype-ddd-pilot/seed/vitest.setup.unit.ts
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom/vitest';
+import { afterAll, afterEach, beforeAll } from 'vitest';
+import { setupServer } from 'msw/node';
+import { handlers } from './__tests__/_msw/handlers';
+
+export const server = setupServer(...handlers);
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
